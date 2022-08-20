230d0855c421ce4a5bb10ee537379894cac688dd
TatianaFlorez <tatiana_florez@usmp.pe>
2022-08-20 17:52:41 -0500

    primer commit

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ejemplo.Models;
+
+namespace webapp.Controllers
+{
+    
+    public class CalculadoraController : Controller
+    {
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Calcular(Calculadora objCalculadora)
+        {
+            double resultado =0.0;
+            
+            if(objCalculadora.Operando =="+"){
+
+                resultado = objCalculadora.Operador1 + objCalculadora.Operador2;
+            }else if(objCalculadora.Operando =="-"){
+                resultado = objCalculadora.Operador1-objCalculadora.Operador2;
+            }else if(objCalculadora.Operando == "*"){
+                resultado = objCalculadora.Operador1*objCalculadora.Operador2;
+            }else {
+                resultado = objCalculadora.Operador1/objCalculadora.Operador2;
+            }
+            ViewData["Message"] = "el resultado es:"+ resultado;
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ejemplo.Models
+{
+    public class Calculadora
+    {
+        public int Operador1 {get;set;}
+        public int Operador2 {get;set;}
+
+        public string Operando {get;set;}
+        
+    }
+}
